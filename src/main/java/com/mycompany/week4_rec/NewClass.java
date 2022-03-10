99a8ffe135a4b1ef87704ce6b7f4fda8d09f47c3
hamzashahnawaz <hamzashahnawaz@localhost>
2022-03-10 13:08:38 -0500

    init repo

---
+++ src/main/java/com/mycompany/week4_rec/NewClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.week4_rec;
+
+
+public class NewClass {
+    
+    void countDown(int num)
+
+ {
+
+      if (num == 0) 
+
+                  System.out.println("Blastoff!");
+
+       else {
+
+                  System.out.println("...");
+
+                    countDown(num-1);
+
+             } 
+
+}
+}
